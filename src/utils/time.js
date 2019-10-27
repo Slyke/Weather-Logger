b6fa2afa65e82a86f202fa958d3c09fab9f74434
Steven Lawler <steven.lawler777@gmail.com>
2019-10-26 20:29:22 -0700

    Fixed another typo

--- src/utils/time.js
+++ src/utils/time.js
@@ -14,10 +14,9 @@ const getYearMonthDayHourMinuteSecond = (currentDate) => {
   let date = currentDate || new Date();
   const offset = date.getTimezoneOffset();
   date = new Date(date.getTime() + (offset*60*1000));
-  date.toISOString().split('T')[0];
 
   // return `${pad(date.getFullYear(), 4)}-${pad(date.getMonth() + 1, 2)}-${pad(date.getDate(), 2)}_${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`;
-  return `${date} ${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`;
+  return `${date.toISOString().split('T')[0]} ${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`;
 };
 
 module.exports = {
