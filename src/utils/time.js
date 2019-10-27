4056acb63c5d52b3fd82ad13c89fb2af9018ce37
Steven Lawler <steven.lawler777@gmail.com>
2019-10-26 20:10:04 -0700

    iso string test

--- src/utils/time.js
+++ src/utils/time.js
@@ -14,7 +14,7 @@ const getYearMonthDayHourMinuteSecond = (currentDate) => {
   const date = currentDate || new Date();
 
   // return `${pad(date.getFullYear(), 4)}-${pad(date.getMonth() + 1, 2)}-${pad(date.getDate(), 2)}_${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`;
-  return date.toISOString();
+  return `date.toISOString()`;
 };
 
 module.exports = {
