2f340965b88cb6e7981e4b1fa561a06182c33d4c
Steven Lawler <steven.lawler777@gmail.com>
2019-10-26 20:55:05 -0700

    Fixed typo

--- src/utils/time.js
+++ src/utils/time.js
@@ -12,7 +12,7 @@ const getYearMonth = (currentDate) => {
 
 const getYearMonthDayHourMinuteSecond = (currentDate) => {
   const date = currentDate || new Date();
-  const returnString = `${pad(date.getFullYear(), 4)}-${pad(date.getMonth() + 1, 2)}-${pad(date.getDate(), 2)} ${pad(date.getMinutes(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`
+  const returnString = `${pad(date.getFullYear(), 4)}-${pad(date.getMonth() + 1, 2)}-${pad(date.getDate(), 2)} ${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`
 console.log(returnString);
   return returnString.toString();
 };
