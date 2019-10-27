896f283819a968e73b45c086e96e15011cca2f46
Steven Lawler <steven.lawler777@gmail.com>
2019-10-26 21:05:06 -0700

    Added more tostrings

--- src/utils/pad.js
+++ src/utils/pad.js
@@ -1,12 +1,12 @@
 function pad(str, totalSize, withChar = "0", atEnd = false) {
-  var s = str + "";
+  var s = str.toString();
   if (atEnd) {
     while (s.length < totalSize) {
-      s += withChar.toString();
+      s = `${s.toString()}${withChar.toString()}`;
     }
   } else {
     while (s.length < totalSize) {
-      s =+ withChar.toString();
+      s = `${withChar.toString()}${s.toString()}`;
     }
   }
   return s;
