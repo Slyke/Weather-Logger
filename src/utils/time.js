5a6bb355e28b4b9e007af29591f97b771ba8d348
Steven Lawler <steven.lawler777@gmail.com>
2019-10-26 20:17:23 -0700

    Trying work around

--- src/utils/time.js
+++ src/utils/time.js
@@ -12,9 +12,12 @@ const getYearMonth = (currentDate) => {
 
 const getYearMonthDayHourMinuteSecond = (currentDate) => {
   const date = currentDate || new Date();
-
+  const offset = date.getTimezoneOffset();
+  date = new Date(date.getTime() + (offset*60*1000));
+  date.toISOString().split('T')[0];
+  
   // return `${pad(date.getFullYear(), 4)}-${pad(date.getMonth() + 1, 2)}-${pad(date.getDate(), 2)}_${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`;
-  return `${date.toISOString()}`;
+  return `${date} ${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`;
 };
 
 module.exports = {
