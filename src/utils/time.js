6ff846edb5a463d03afe00f21df857193eeb7f13
Steven Lawler <steven.lawler777@gmail.com>
2019-10-26 20:44:06 -0700

    Added quotes around date and time

--- src/utils/time.js
+++ src/utils/time.js
@@ -12,12 +12,8 @@ const getYearMonth = (currentDate) => {
 
 const getYearMonthDayHourMinuteSecond = (currentDate) => {
   const date = currentDate || new Date();
-  const offset = date.getTimezoneOffset();
-  const convertedDate = new Date(date.getTime() + (offset * 60 * 1000));
-  const timeStr = `0${date.getHours()}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`
 
-  // return `${pad(date.getFullYear(), 4)}-${pad(date.getMonth() + 1, 2)}-${pad(date.getDate(), 2)}_${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}`;
-  return `${convertedDate.toISOString().split('T')[0]} ${timeStr}`;
+  return `"${pad(date.getFullYear(), 4)}-${pad(date.getMonth() + 1, 2)}-${pad(date.getDate(), 2)}"_"${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}"`;
 };
 
 module.exports = {
